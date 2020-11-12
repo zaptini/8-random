a50bcb55123b3b60ac5c2ebf5818df88e7924d4c
Martin <zaptini@gmail.com>
2020-11-12 08:21:58 +0200

    Update random.js

--- random.js
+++ random.js
@@ -1,3 +1,4 @@
+//pieci skaitļi līdz 35
 var cipari=[]
 let rnd = 0
 for (var i=0; i<9000000; i++) {
@@ -32,29 +33,78 @@ sortable.forEach(function(item){
 
 sortable.reverse()
 
-for (i=0; i<8; i++) {
-    console.log(sortable[i])
+// for (i=0; i<8; i++) {
+//     console.log(sortable[i])
+// }
+
+
+n0=parseInt(sortable[0])
+n1=parseInt(sortable[9])
+n2=parseInt(sortable[21])
+n3=parseInt(sortable[17])
+n4=parseInt(sortable[1])
+
+//2 skaitļi līdz 10
+var syortable = []
+var cyipari=[]
+let rynd = 0
+for (var i=0; i<9000000; i++) {
+    rnd = Math.floor(Math.random()*10+1);
+    cyipari.push(rnd);
+    };
+
+a = cyipari
+ryesult = { };
+for(var i = 0; i < a.length; ++i) {
+    if(!ryesult[a[i]])
+        ryesult[a[i]] = 0;
+    ++ryesult[a[i]];
+};
+// console.log(result)
+
+var syrtable = [];
+for (var vyehicle in ryesult) {
+    syortable.push([vyehicle, ryesult[vyehicle]]);
 }
 
-// var n0=sortable[0]
-n0=parseInt(sortable[0])
-n1=parseInt(sortable[1])
-n2=parseInt(sortable[2])
-n3=parseInt(sortable[3])
-n4=parseInt(sortable[4])
-n5=parseInt(sortable[5])
-n6=parseInt(sortable[6])
-n7=parseInt(sortable[7])
+syortable.sort(function(a, b) {
+    return a[1] - b[1];
+});
 
+// console.log(sortable)
+
+var objSorted = {}
+syortable.forEach(function(item){
+    objSorted[item[0]]=item[1]
+})
+
+syortable.reverse()
+
+// for (zi=0; zi<8; zi++) {
+//     console.log(syortable[zi])
+// }
+
+
+ny0=parseInt(syortable[0])
+ny1=parseInt(syortable[4])
+
+//Izvade uz ekrānu
 
 var h = document.createElement("H1");
-var t = document.createTextNode("Eight random numbers are: ");
-h.appendChild(t); // Append the text node to the H1 element
+var t = document.createTextNode("Lucky numbers are: ");
+h.appendChild(t); 
+document.body.appendChild(h); 
 
-document.body.appendChild(h); // Append the H1 element to the document body
+var hi = document.createElement("H2");
+var ti = document.createTextNode(n0 + " " + n1 +" "+ n2 +" "+ n3 +" "+ n4 +" ");
+hi.appendChild(ti); 
+document.body.appendChild(hi); 
 
-var h = document.createElement("H2");
-var t = document.createTextNode(n0 + " " + n1 +" "+ n2 +" "+ n3 +" "+ n4 +" "+ n5 +" "+ n6 +" "+ n7 +" ");
-h.appendChild(t); // Append the text node to the H2 element
 
-document.body.appendChild(h); // Append the H1 element to the document body
+// document.body.appendChild(h);
+
+
+var hy = document.createElement("H3");
+var ty = document.createTextNode(ny0 + " " + ny1 +" ");
+hy.appendChild(ty); 
+document.body.appendChild(hy); 
